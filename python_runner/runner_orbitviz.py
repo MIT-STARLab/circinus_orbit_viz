d74c95c326f72c2db1e082dd32c2ac392f64814c
Kit Kennedy <akennedy@mit.edu>
2018-06-13 23:06:41 -0400

    small updates

--- python_runner/runner_orbitviz.py
+++ python_runner/runner_orbitviz.py
@@ -111,7 +111,7 @@ class PipelineRunner:
         sat_ids = None
         gs_ids = None
 
-        if orbit_prop_inputs['version'] == "0.6":
+        if orbit_prop_inputs['version'] == "0.7":
             scenario_params = orbit_prop_inputs['scenario_params']
 
             start_utc_dt =tt.iso_string_to_dt (scenario_params['start_utc'] ) 
